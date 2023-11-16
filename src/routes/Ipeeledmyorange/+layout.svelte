1fefe6c065f2ee7ff28279d1efcb58d073d54f4d
Sydney Bruce <sydneybruce20@gmail.com>
2023-11-16 14:37:16 -0700

    added third page styling

--- src/routes/Ipeeledmyorange/+layout.svelte
+++ src/routes/Ipeeledmyorange/+layout.svelte
@@ -3,22 +3,26 @@ import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
 let boxItem = [
 	{name:"Books", value:"books", valuesingle: "books"},
 	{name:"Movies", value:"movies", valuesingle: "movies"},
-	{name: 'TV', value:"tv", valuesingle: "movies"}
+	{name: 'TV', value:"tv", valuesingle: "movies"},
+	{name: 'more', value:"more", valuesingle: "more"},
+	{name: 'stuff', value:"stuff", valuesingle: "stuff"},
+	{name: 'here', value:"here", valuesingle: "here"}
 ]
 </script>
 
-<main class="m-4">
-	<h4 class="text-2xl mr-8 text-center">OUR PRODUCTS</h4>
-	<div class="max-w-max bg-tertiary-800 p-4 rounded-lg m-4">
-	<ListBox>
-		{#each boxItem as {name, value, valueSingle}}
-		<ListBoxItem bind:group={valueSingle} name="medium" value={value}>{name}</ListBoxItem>
-		{/each}
-	</ListBox>
+<main class="m-4 flex ">
+	<div>
+		<div class="max-w-max bg-tertiary-800 p-4 rounded-lg m-4 self-center min-h-screen">
+			<ListBox>
+				{#each boxItem as {name, value, valueSingle}}
+				<ListBoxItem bind:group={valueSingle} name="medium" value={value}>{name}</ListBoxItem>
+				{/each}
+			</ListBox>
+		</div>
+</div>
+	<div>
+		<slot />
 	</div>
-
-
-	<slot />
 
 </main>
 
--- src/routes/Ipeeledmyorange/+page.svelte
+++ src/routes/Ipeeledmyorange/+page.svelte
@@ -1,10 +1,16 @@
 <script>
   let cardItems = [
-    {heading: "product 1", src:"./assets/cards/01-cardimg.jpg", alt: "yellow backpack"}
+    {heading: "product 1", src:"./assets/cards/01-cardimg.jpg", alt: "yellow backpack"},
+    {heading: "product 2", src:"./assets/cards/02-cardimg.jpg", alt: "many backpacks"},
+    {heading: "product 3", src:"./assets/cards/03-cardimg.jpg", alt: "cup"},
+    {heading: "product 4", src:"./assets/cards/04-cardimg.jpg", alt: "yellow gloves"},
+    {heading: "product 5", src:"./assets/cards/05-cardimg.jpg", alt: "table in water"},
+    {heading: "product 6", src:"./assets/cards/06-cardimg.jpg", alt: "sleeping bag"}
   ]
 </script>
-<div class="mx-2 text-center">
+<h4 class="text-2xl mr-8 text-center">OUR PRODUCTS</h4>
+<div class=" ml-8 text-center flex g-2 flex-wrap m">
   {#each cardItems as {heading, src, alt}}
-<a href="/" class="card"> <div class=" card max-w-max p-4"><img class="w-[200px] h-auto "src="{src}" alt="{alt}">{heading}</div></a>
+<a href="/" class="card m-2"> <div class=" card max-w-max p-4 w-[500px]"><img class="w-[300px] h-auto "src="{src}" alt="{alt}">{heading}</div></a>
   {/each}
 </div>
