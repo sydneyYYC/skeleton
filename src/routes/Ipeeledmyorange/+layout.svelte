5fdacde86d140e9a200022bd4ac49b9662e093f8
Sydney Bruce <sydneybruce20@gmail.com>
2023-11-16 14:18:11 -0700

    added card images to assets/card

--- src/routes/Ipeeledmyorange/+layout.svelte
+++ src/routes/Ipeeledmyorange/+layout.svelte
@@ -1,6 +1,25 @@
 <script>
-
+import { ListBox, ListBoxItem } from '@skeletonlabs/skeleton';
+let boxItem = [
+	{name:"Books", value:"books", valuesingle: "books"},
+	{name:"Movies", value:"movies", valuesingle: "movies"},
+	{name: 'TV', value:"tv", valuesingle: "movies"}
+]
 </script>
 
-<slot />
+<main class="m-4">
+	<h4 class="text-2xl mr-8 text-center">OUR PRODUCTS</h4>
+	<div class="max-w-max bg-tertiary-800 p-4 rounded-lg m-4">
+	<ListBox>
+		{#each boxItem as {name, value, valueSingle}}
+		<ListBoxItem bind:group={valueSingle} name="medium" value={value}>{name}</ListBoxItem>
+		{/each}
+	</ListBox>
+	</div>
+
+
+	<slot />
+
+</main>
+
 
---
+++ src/routes/Ipeeledmyorange/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  let cardItems = [
+    {heading: "product 1", src:"./assets/cards/01-cardimg.jpg", alt: "yellow backpack"}
+  ]
+</script>
+<div class="mx-2 text-center">
+  {#each cardItems as {heading, src, alt}}
+<a href="/" class="card"> <div class=" card max-w-max p-4"><img class="w-[200px] h-auto "src="{src}" alt="{alt}">{heading}</div></a>
+  {/each}
+</div>
